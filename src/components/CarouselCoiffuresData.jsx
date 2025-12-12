6f1e106dafdf104733483ef1f5af29ae0397cedc
ccuvillier <c.cuvillier@gmail.com>
2025-12-12 17:30:49 +0100

    Ajout de cheveux

--- src/components/CarouselCoiffuresData.jsx
+++ src/components/CarouselCoiffuresData.jsx
@@ -1,9 +1,18 @@
 import CheveuxAnglaises from "./images/CheveuxAnglaises.jsx";
 import CheveuxChignon from "./images/cheveuxChignon.jsx";
 import CheveuxFrises from "./images/cheveuxFrises.jsx";
+import CheveuxCouettes from "./images/cheveuxCouettes.jsx";
+import CheveuxRaides from "./images/cheveuxRaides.jsx";
+import CheveuxCarre from "./images/cheveuxCarre.jsx";
+import CheveuxCouetteCote from "./images/cheveuxCouetteCote.jsx";
 
 export const hairs = [
   { name: "CheveuxAnglaises", component: CheveuxAnglaises },
   { name: "CheveuxChignon", component: CheveuxChignon },
-  { name: "CheveuxFrises", component: CheveuxFrises }
+  { name: "CheveuxFrises", component: CheveuxFrises },
+  { name: "CheveuxCouettes", component: CheveuxCouettes },
+  { name: "CheveuxRaides", component: CheveuxRaides },
+  { name: "CheveuxCarre", component: CheveuxCarre },
+  { name: "CheveuxCouetteCote", component: CheveuxCouetteCote }
 ];
+
